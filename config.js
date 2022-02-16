8b78c6691b2be9c52d19a6ec4d17fc48f05fa4db
BotGua <74773543+BotGua@users.noreply.github.com>
2022-02-16 22:53:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+global.packName = '𝙺𝙸𝚉𝙰𝙺𝙸𝚇𝙳'
+global.authorName = 'Yotsuba - Bot'
+global.prefix = '#'
+global.mode = 'publik'
+global.ownerNumber = ['687813379','6285878313791']
+global.thumb = fs.readFileSync('./thumb.jpeg')
+
+global.APIs = {
+	zaki: 'https://api.kizakixd.xyz/api'
+}
+global.APIKeys = {
+	'https://api.kizakixd.xyz/api': 'Your_Apikey' //register buat dapetin api di : https://api.kizakixd.xyz/api
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(`[UPDATE] '${__filename}'`)
+    delete require.cache[file]
+    require(file)
+})
